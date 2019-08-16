3e63de1d073f24d94246e61f1a1a5c4b27989c96
Melissa Williams <melissa@brandmeetscopy.com>
2019-08-16 23:52:41 +0000

    Automatically backed up by Learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,16 @@
 class Pirate
+
+  attr_reader :name, :weight, :height
+  @@pirates = []
+
+  def initialize(params)
+    @name = params[:name]
+    @height = params[:height]
+    @weight = params[:weight]
+    @@pirates << self
+  end
+
+  def self.all
+    @@pirates
+  end
 end
