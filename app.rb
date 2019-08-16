14692fb4a02164404bde7cec10ba56b8db7729e7
Melissa Williams <melissa@brandmeetscopy.com>
2019-08-16 23:53:20 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -15,5 +15,17 @@ post '/pirates' do
   erb :pirates
   end
 
+  post '/pirates' do
+  @pirate = Pirate.new(params[:pirate])
+ 
+  params[:pirates][:ships].each do |details|
+    Ship.new(details)
+  end
+ 
+  @ships = Ship.all
+ 
+  erb :student
+end
+
   end
 end
