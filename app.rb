0440bcada5b03399bee814c4e38c93e5b8ab1d31
Melissa Williams <melissa@brandmeetscopy.com>
2019-08-17 01:03:23 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -10,11 +10,11 @@ module FormsLab
   get '/new' do
     erb :'pirates/new'
   end
-
-  get '/pirates' do
-    @pirates = Pirate.all
-    erb :'pirates/new'
-  end
+  # 
+  # get '/pirates' do
+  #   @pirates = Pirate.all
+  #   erb :'pirates/new'
+  # end
 
   post '/pirates' do
   @pirate = Pirate.new(params[:pirate])
