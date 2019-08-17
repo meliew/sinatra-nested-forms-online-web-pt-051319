c8fadbe094bb20cfedb4f3b21218531c31f5dcc8
Melissa Williams <melissa@brandmeetscopy.com>
2019-08-17 01:07:43 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,24 +3,24 @@ require './environment'
 module FormsLab
   class App < Sinatra::Base
 
-  get '/' do
-    erb :index
-  end
+    get '/' do
+      erb :index
+    end
+    
+    get '/new' do
+      erb :'pirates/new'
+    end
 
-  get '/new' do
-    erb :'pirates/new'
-  end
+    post '/pirates' do
+      @pirate = Pirate.new(params[:pirate])
 
-  post '/pirates' do
-  @pirate = Pirate.new(params[:pirate])
+      params[:pirate][:ships].each do |details|
+        Ship.new(details)
+      end
+      @ships = Ship.all
 
-  params[:pirate][:ships].each do |details|
-    Ship.new(details)
-  end
-  @ships = Ship.all
-
-  erb :'pirates/show'
-end
+      erb :'pirates/show'
+    end
 
   end
 end
